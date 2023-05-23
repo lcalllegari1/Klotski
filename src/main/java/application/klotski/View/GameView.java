1620d3b68bc1a9dcb0fdd0dd669101340f41d6b5
lcallegari <leonardo.callegari.1@studenti.unipd.it>
2023-05-23 20:55:04 +0200

    Updated MenuView graphics. Created GameView.fxml & GameView.java (Pseudo controller)

---
+++ src/main/java/application/klotski/View/GameView.java
@@ -0,0 +1,16 @@
+package application.klotski.View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
